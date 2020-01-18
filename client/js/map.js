816a3fd54c5ae928fcbe49668bcd3f3c23ff520f
Harry <honeytung@gmail.com>
2020-01-18 14:38:57 -0800

    Added Original API Function

--- client/js/map.js
+++ client/js/map.js
@@ -1,4 +1,8 @@
 // Simple Google Map API
-function carMap() {
-    var mapProp = new google.maps.Map(document.getElementById('googleMap'), {center: {lat: 49.246292, lng: -123.116226}, zoom: 8} 
-    );
+function myMap() {
+    var mapProp = {
+        center: new google.maps.LatLng(51.508742, -0.120850),
+        zoom: 5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
