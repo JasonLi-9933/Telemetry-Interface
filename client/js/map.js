e2aaf1f5f294b47220ca7be1237943e5904e207e
chrrisa18 <azchris00@gmail.com>
2020-01-18 12:40:31 -0700

    Changes to map api

--- client/js/map.js
+++ client/js/map.js
@@ -1,8 +1,5 @@
 // Simple Google Map API
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(51.508742, -0.120850),
-        zoom: 5,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
+function carMap() {
+    var mapProp = new google.maps.Map(document.getElementById('map'), {center: {lat: 49.246292, lng: -123.116226}, zoom: 8} 
+    );
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDwiKfX0sN-gixmxaJ49xK8cwNo88jTtII&callback=initMap"async defer> </script>
